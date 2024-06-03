7d46014d04e5840a3331cc76bdc19dbdeecf9587
parvez13913 <parvezz13913@gmail.com>
2024-06-03 21:29:51 +0600

    [DONE] Project setup

---
+++ src/app/modules/payment/payment.controller.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import { PaymentServiec } from "./payment.service";
+
+const initPayment = async (req: Request, res: Response, Next: NextFunction) => {
+  const result = await PaymentServiec.initPayment();
+  res.send(result)
+}
+
+
+
+export const PaymentController = {
+  initPayment
+}
---
+++ src/app/modules/payment/payment.route.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { PaymentController } from './payment.controller';
+
+const router = express.Router();
+
+router.post("/", PaymentController.initPayment)
+
+
+
+
+
+export const PaymentRoutes = router
